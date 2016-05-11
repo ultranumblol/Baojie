eed40c1217649c3afcae66e7b1aa9e190014a1c4
wgz <490480770@qq.com>
2016-05-11 16:36:45 +0800

    5/11

--- app/src/main/java/com/ant/wgz/fragment/Fragment2.java
+++ app/src/main/java/com/ant/wgz/fragment/Fragment2.java
@@ -1,6 +1,12 @@
 package com.ant.wgz.fragment;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -14,6 +20,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.ant.wgz.MainActivity;
 import com.ant.wgz.OrderActivity;
 import com.ant.wgz.R;
 import com.ant.wgz.adapter.OrderRecyclerViewAdapter;
@@ -80,8 +87,32 @@ public class Fragment2 extends Fragment {
         @Override
         protected void onPostExecute(Object o) {
             mrefresh.setRefreshing(false);
+            RefreshRadio();
             Snackbar.make(root,"刷新成功！",Snackbar.LENGTH_SHORT).show();
             super.onPostExecute(o);
         }
+    }
+
+    public void RefreshRadio(){
+        NotificationManager manager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent pendingIntent3 = PendingIntent.getActivity(getActivity(), 0,
+                new Intent(getActivity(), MainActivity.class), 0);
+        Uri ringUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        // 通过Notification.Builder来创建通知，注意API Level
+        // API16之后才支持
+        Notification notify3 = null; // 需要注意build()是在API
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            notify3 = new Notification.Builder(getActivity())
+                   /* .setSmallIcon(R.drawable.mabu)
+                    .setTicker("TickerText:" + "您有新短消息，请注意查收！")
+                    .setContentTitle("Notification Title")
+                    .setContentText("This is the notification message")*/
+                    /*.setContentIntent(pendingIntent3)*/.setSound(ringUri).build();
+        }
+        // level16及之后增加的，API11可以使用getNotificatin()来替代
+        notify3.flags |= Notification.FLAG_AUTO_CANCEL; // FLAG_AUTO_CANCEL表明当通知被用户点击时，通知将被清除。
+        manager.notify(1, notify3);// 步骤4：通过通知管理器来发起通知。如果id不同，则每click，在status哪里增加一个提示
+
+
     }
 }
